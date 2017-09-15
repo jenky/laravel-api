706ff9dc1f4843f1931e6a099a41a2f79ccc4b3d
Lynh Tran <jenky.w0w@gmail.com>
2017-09-15 17:07:18 +0700

    Clean up

--- src/Exception/Handler.php
+++ src/Exception/Handler.php
@@ -17,20 +17,6 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 class Handler extends IlluminateExceptionHandler implements ExceptionHandler, ExceptionHandlerContract
 {
     /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
-    ];
-
-    /**
      * Array of exception handlers.
      *
      * @var array
