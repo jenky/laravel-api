883283c44835c7660fd5f9deac5c682508efbcf6
Lynh Tran <jenky.w0w@gmail.com>
2019-10-08 23:45:39 +0700

    test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -4,7 +4,31 @@ use Jenky\LaravelAPI\Test\FeatureTestCase;
 
 class ExampleTest extends FeatureTestCase
 {
-    public function test_example()
+    public function test_api_v1_prefix()
+    {
+        $this->getJson('/api/v1')
+            ->assertStatus(200)
+            ->assertJson([
+                'version' => [
+                    'set' => 'v1',
+                    'route' => 'v1',
+                ],
+            ]);
+    }
+
+    public function test_api_v2_prefix()
+    {
+        $this->getJson('/api/v2')
+            ->assertStatus(200)
+            ->assertJson([
+                'version' => [
+                    'set' => 'v2',
+                    'route' => 'v2',
+                ],
+            ]);
+    }
+
+    public function test_example_3()
     {
         $this->assertTrue(true);
     }
--- tests/FeatureTestCase.php
+++ tests/FeatureTestCase.php
@@ -7,6 +7,24 @@ use Orchestra\Testbench\TestCase;
 
 class FeatureTestCase extends TestCase
 {
+    /**
+     * Setup the test environment.
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        require 'routes.php';
+    }
+
+    /**
+     * Get base path.
+     *
+     * @return string
+     */
+    // protected function getBasePath()
+    // {
+    //     return __DIR__.'/../../';
+    // }
+
     protected function getPackageProviders($app)
     {
         return [
