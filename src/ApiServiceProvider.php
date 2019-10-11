31e96238bd79986e86d4c546d0b68f0a2863b02a
Lynh Tran <jenky.w0w@gmail.com>
2019-10-11 15:28:30 +0700

    clean up

--- src/ApiServiceProvider.php
+++ src/ApiServiceProvider.php
@@ -5,20 +5,14 @@ namespace Jenky\LaravelAPI;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Jenky\LaravelAPI\Contracts\Http\Validator;
 use Jenky\LaravelAPI\Contracts\Http\VersionParser;
-use Jenky\LaravelAPI\Http\Validator\DomainValidator;
-use Jenky\LaravelAPI\Http\Validator\PrefixValidator;
 use Jenky\LaravelAPI\Http\Validator\ValidatorManager;
-use Jenky\LaravelAPI\Http\VersionParser\Header;
-use Jenky\LaravelAPI\Http\VersionParser\Uri;
 use Jenky\LaravelAPI\Http\VersionParser\VersionParserManager;
 use Jenky\LaravelAPI\Macros\RequestMacros;
 use Jenky\LaravelAPI\Macros\ResponseMacros;
 use Jenky\LaravelAPI\Macros\RouteMacros;
 use Jenky\LaravelAPI\Macros\RouterMacros;
-use RuntimeException;
 
 class ApiServiceProvider extends ServiceProvider
 {
@@ -82,57 +76,9 @@ class ApiServiceProvider extends ServiceProvider
      */
     protected function registerRequestValidator()
     {
-        // TODO: clean up
-        // $method = 'register'.Str::studly($this->config('uri_scheme')).'Validator';
-
-        // if (method_exists($this, $method)) {
-        //     $this->{$method}();
-        // } else {
-        //     throw new RuntimeException('Invalid API scheme configuration.');
-        // }
         $this->app->singleton(Validator::class, function ($app) {
             return new ValidatorManager($app);
         });
-    }
-
-    /**
-     * Bind the request validator implementation to the application container.
-     *
-     * @param  \Jenky\LaravelAPI\Contracts\Http\Validator $validator
-     * @return void
-     */
-    protected function bindRequestValidatorToContainer(Validator $validator)
-    {
-        // TODO: removed
-        $this->app->singleton(Validator::class, function () use ($validator) {
-            return $validator;
-        });
-    }
-
-    /**
-     * Register the package prefix validator.
-     *
-     * @return void
-     */
-    protected function registerPrefixValidator()
-    {
-        // TODO: removed
-        $this->bindRequestValidatorToContainer(
-            new PrefixValidator($this->config('prefix'))
-        );
-    }
-
-    /**
-     * Register the package domain validator.
-     *
-     * @return void
-     */
-    protected function registerDomainValidator()
-    {
-        // TODO: removed
-        $this->bindRequestValidatorToContainer(
-            new DomainValidator($this->config('domain'))
-        );
     }
 
     /**
@@ -143,56 +89,9 @@ class ApiServiceProvider extends ServiceProvider
      */
     protected function registerVersionParser()
     {
-        // TODO: clean up
-        // $method = 'register'.Str::studly($this->config('version_scheme')).'VersionParser';
-
-        // if (method_exists($this, $method)) {
-        //     $this->{$method}();
-        // } else {
-        //     throw new RuntimeException('Invalid version scheme configuration.');
-        // }
         $this->app->singleton(VersionParser::class, function ($app) {
             return new VersionParserManager($app);
         });
-    }
-
-    /**
-     * Bind the version parser implementation to the application container.
-     *
-     * @param  \Jenky\LaravelAPI\Contracts\Http\VersionParser $parser
-     * @return void
-     */
-    protected function bindVersionParserToContainer(VersionParser $parser)
-    {
-        // TODO: removed
-        $this->app->singleton(VersionParser::class, function () use ($parser) {
-            return $parser;
-        });
-    }
-
-    /**
-     * Register the package header version parser.
-     *
-     * @return void
-     */
-    protected function registerHeaderVersionParser()
-    {
-        // TODO: removed
-        $this->bindVersionParserToContainer(
-            new Header($this->app['config'])
-        );
-    }
-
-    /**
-     * Register the package URI version parser.
-     *
-     * @return void
-     */
-    protected function registerUriVersionParser()
-    {
-        // TODO: removed
-
-        $this->bindVersionParserToContainer(new Uri);
     }
 
     /**
